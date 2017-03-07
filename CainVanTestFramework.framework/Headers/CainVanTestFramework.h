c4db96e67cbf6b6637f1268fcb8a543263b66e55
Cain <cain@Cain.local>
2017-03-07 17:45:25 +0800

    上传测试framework

---
+++ CainVanTestFramework.framework/Headers/CainVanTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  CainVanTestFramework.h
+//  CainVanTestFramework
+//
+//  Created by Cain on 2017/3/7.
+//  Copyright © 2017年 cmos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CainVanTestFramework.
+FOUNDATION_EXPORT double CainVanTestFrameworkVersionNumber;
+
+//! Project version string for CainVanTestFramework.
+FOUNDATION_EXPORT const unsigned char CainVanTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CainVanTestFramework/PublicHeader.h>
+#import <CainVanTestFramework/Hello.h>
+
---
+++ CainVanTestFramework.framework/Headers/Hello.h
@@ -0,0 +1,15 @@
+//
+//  Hello.h
+//  CainVanTestFramework
+//
+//  Created by Cain on 2017/3/7.
+//  Copyright © 2017年 cmos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hello : NSObject
+
+- (void)SayHello;
+
+@end
